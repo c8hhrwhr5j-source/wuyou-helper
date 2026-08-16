f30ee681b33fe787889e835cb64f136f6fa5fd1f
c8hhrwhr5j-source <c8hhrwhr5j@privaterelay.appleid.com>
2026-08-17 01:28:04 +0800

    fix: correct landscape notice button coords (959,164)->(959,165) to match portrait pixel (585,959)

--- main.lua
+++ main.lua
@@ -7,22 +7,29 @@ function main()
     log(string.format("屏幕 %.0f x %.0f", w, h))
     while true do
         sleep(1000)
+
+        if getColor(563, 1280) == 0xA2A2A2 then
+            click(563, 1280)
+            sleep(1000)
+            log("找到坚屏灰色设置按钮点击")
+        end
+
         if getColor(1280, 187) == 0xA2A2A2 then
             click(1280, 187)
             sleep(1000)
-            log("找到灰色设置按钮点击")
+            log("找到横屏灰色设置按钮点击")
         end
 
-        if getColor(958, 163) == 0xB53431 then
-            click(958, 163)
+        if getColor(585, 959) == 0xB53029 then
+            click(585, 959)
             sleep(1000)
-            log("找到公告关闭按钮点击")
+            log("找到坚屏公告关闭按钮点击")
         end
 
-        if getColor(586, 959) == 0xB52C29 then
-            click(586, 959)
+        if getColor(959, 165) == 0xB53029 then
+            click(959, 165)
             sleep(1000)
-            log("找到公告关闭按钮点击1")
+            log("找到横屏公告关闭按钮点击1")
         end
     end
 end
@@ -49,5 +56,6 @@ function init(n)
     screen.init(n)   -- 脚本坐标系 0 = home 在下，1 = home 在右，2 = home 在左
 end
 
+init(1)
 
 main()
